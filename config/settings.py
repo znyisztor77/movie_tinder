27401b1caeafa2978e1ccfcc6ed539f230b1d582
nyisztor.zoltan <znyisztor77@gmail.com>
2024-01-15 18:51:44 +0100

    01.15

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,9 @@ from .secret  import KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#MIME
+import mimetypes
+mimetypes.add_type("application/javascript", ".js", True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -56,7 +59,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'frontend/dist/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
