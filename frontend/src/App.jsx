c5ce09ea7cad9242a729dfc93e7f16920cb01d0a
Takács <TakcsBela@gmail.com>
2024-02-05 19:16:55 +0100

    orai munka

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React ,{ useEffect, useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+const App = () => {
+  const[movies, setMovies] = useState([])
+  const[movieIndex, setMovieIndex] = useState(0)
+
+  useEffect(()=>{
+    fetch('http://127.0.0.1:8000/api/movies/')
+    .then(res => res.json())
+    .then(apiMovies=> {
+      setMovies(apiMovies)
+      console.log(apiMovies)
+    })
+  }, [])
+
+  const generateImageUrl = (old) => {
+    return "http://127.0.0.1:8000" + old
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+
+    {movies.length > 0 ?
+        <div className="card" style={{ backgroundImage: `url(${generateImageUrl(movies[movieIndex].poster_image)})` }}>
+          <h2>{ movies[movieIndex].title }</h2>
+        </div> :
+        <div>Loading...</div>}
+
+    <div className='buttons'>
+      <button>👍</button>
+      <button>😎</button>
+    </div>
+
+    </div>
   )
 }
 
